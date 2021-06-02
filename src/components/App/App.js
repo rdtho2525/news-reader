bfd10c3eb8a0596d8e94b00e651d030dd11fc6bd
Reggie Thompson <reggie.thompson4@gmail.com>
2021-06-02 15:41:33 -0600

    Add articles, error to state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,25 @@
+import { useState, useEffect } from 'react'
 import './App.css';
 import 'normalize.css';
+import { fetchUSStories } from '../../api';
+
 function App() {
+  const [ articles, setArticles ] = useState([]);
+  const [ error, setError ] = useState('');
+
+  const getArticles = async () => {
+    try {
+      const stories = await fetchUSStories();
+      setArticles(stories);
+    } catch {
+      setError('We\'re sorry, something went wrong! Please try again later.')
+    }
+  }
+
+  useEffect(() => {
+    getArticles();
+  } , []);
+
   return (
     <>
     </>
