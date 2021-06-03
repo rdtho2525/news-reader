5e7a49d2d26378428aa1c8c33deaf0fbf50f5e8d
Reggie Thompson <reggie.thompson4@gmail.com>
2021-06-02 20:53:59 -0600

    Convert article tags to Links

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,14 +1,15 @@
 import FeaturedArticle from '../FeaturedArticle/FeaturedArticle'
 import ArticleCard from '../ArticleCard/ArticleCard'
+import { Link } from 'react-router-dom';
 import './Dashboard.css';
 
 const Dashboard = ({ articles }) => {
   const featuredArticle = articles.map((article, i) => {
     if (i === 0) {
       return (
-        <article className="dashboard_featured-article">
+        <Link to="" className="dashboard_featured-article">
           <FeaturedArticle article={article}/>
-        </article>
+        </Link>
       )
     } 
   });
@@ -16,9 +17,9 @@ const Dashboard = ({ articles }) => {
   const displayedArticles = articles.map((article, i) => {
     if (i > 0 && i < 5) {
       return (
-        <article className="dashboard_article">
+        <Link to="" className="dashboard_article">
           <ArticleCard article={article}/>
-        </article>
+        </Link>
       )
     }
   });
@@ -27,7 +28,6 @@ const Dashboard = ({ articles }) => {
     <>
       <section className="dashboard_featured-section">
         {featuredArticle}
-        {/* {console.log(featuredArticle)} */}
       </section>
       <section className="dashboard_articles-section">
         {displayedArticles}
