1601a0be34e246775ff8f4df8632c118f1d1496a
Reggie Thompson <reggie.thompson4@gmail.com>
2021-06-03 16:26:29 -0600

    Add state to BrowseArticles to toggle results and all articles

--- src/components/BrowseArticles/BrowseArticles.js
+++ src/components/BrowseArticles/BrowseArticles.js
@@ -1,11 +1,20 @@
-import ArticleCard from '../ArticleCard/ArticleCard'
+import { useState, useEffect } from 'react';
+import ArticleCard from '../ArticleCard/ArticleCard';
 import { Link } from 'react-router-dom';
 import './BrowseArticles.css';
 
-const BrowseArticles = ({ articles }) => {
-  console.log('hi')
-  console.log(articles)
-  const displayedStories = articles.map((article, i) => {
+const BrowseArticles = ({ articles, searchResults, message }) => {
+  const [ allArticles, setAllArticles ] = useState([])
+  
+  const determineAllArticles = () => {
+    if (!!searchResults.length) {
+      setAllArticles(searchResults);
+    } else {
+      setAllArticles(articles);
+    }
+  }
+
+  const displayedStories = allArticles.map((article, i) => {
     return (
       <Link to={article.title} className="browse-articles_article" key={`${article.title}-${i}`}>
         <ArticleCard article={article}/>
@@ -13,10 +22,14 @@ const BrowseArticles = ({ articles }) => {
     )
   });
 
+  useEffect(() => {
+    determineAllArticles();
+  }, [searchResults])
+
   return (
     <>
       <div className="browse-articles_content-container">
-        <h2 className="header">Viewing All Stories:</h2>
+        <h2 className="header">{message}</h2>
         <section className="browse-articles_articles-section">
           {displayedStories}
         </section>
