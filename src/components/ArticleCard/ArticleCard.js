5fc6e741b060ef54189dc713d4859e3ad82b0d9b
Reggie Thompson <reggie.thompson4@gmail.com>
2021-06-02 20:53:16 -0600

    Apply structure to ArticleCard

--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -1,8 +1,10 @@
+import './ArticleCard.css';
+
 const ArticleCard = ({ article }) => {
 
   return (
     <>
-      <img className="article-card_img" src={`${article.multimedia[3].url}`} alt='' />
+      <img className="article-card_img" src={article.multimedia[3].url} alt={article.title} />
       <p className="article-card_title">{article.title}</p>
     </>
   )
