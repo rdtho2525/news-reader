506c333585cdfb36211cd9173d9701df69b4930c
Reggie Thompson <reggie.thompson4@gmail.com>
2021-06-03 10:51:17 -0600

    Render search button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,17 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 
-const Header = () => {
+const Header = ({ getSearchResults }) => {
+  // const handleSearch = (e) => {
+  //   getSearchResults(e);
+  // }
+
   return (
     <header className="header-app_header">
       <div className="header-search_container">
         <SearchIcon />
         <input className="header_search-input" type="text" placeholder="Search by Article"></input>
+        <button className="header_search-button">Search</button>
       </div>
       <Link to="/stories" className="header_stories-link">
         View Stories
