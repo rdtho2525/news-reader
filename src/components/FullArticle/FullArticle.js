96b66e0df46051b793951aa1f3e67542f2a95eec
Reggie Thompson <reggie.thompson4@gmail.com>
2021-06-03 08:56:35 -0600

    Move header to the top of the section

--- src/components/FullArticle/FullArticle.js
+++ src/components/FullArticle/FullArticle.js
@@ -19,6 +19,9 @@ const FullArticle = ({ title, articles }) => {
       {currentArticle && (
       <>
         <section className="full-article_section">
+          <h2 className="full-article_title">{currentArticle.title}</h2>
+          <h3 className="full-article_abstract">{currentArticle.abstract}</h3>
+          <h4 className="full-article_byline">{currentArticle.byline}</h4>
           <img className="full-article_img" src={currentArticle.multimedia[0].url} alt={currentArticle.multimedia[0].caption}/>
           <aside className="full-article_photo-caption-container">
             <p className="full-article_photo-caption">{currentArticle.multimedia[0].caption}</p>
@@ -27,10 +30,7 @@ const FullArticle = ({ title, articles }) => {
               <p>Last updated: {currentArticle.updated_date}</p>
             </div>
           </aside>
-          <h2 className="full-article_title">{currentArticle.title}</h2>
-          <h3 className="full-article_abstract">{currentArticle.abstract}</h3>
-          <h4 className="full-article_byline">{currentArticle.byline}</h4>
-          <p className="full-article_external-link">View full story <a href={currentArticle.url}>here</a>:</p>
+          <p className="full-article_link-text">View full story <a className="full-article_external-link" href={currentArticle.url} target="_blank">here</a>:</p>
         </section>
         <footer className="full-article_footer">{currentArticle.copyright}</footer>
       </>
