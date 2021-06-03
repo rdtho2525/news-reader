2bdc961f456116b4330e03bc789c8e6df973f4c5
Reggie Thompson <reggie.thompson4@gmail.com>
2021-06-03 11:27:42 -0600

    Write helper function to setSearchInput

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,14 +3,15 @@ import { fetchUSStories } from '../../api';
 import Dashboard from '../Dashboard/Dashboard.js';
 import Header from '../Header/Header.js';
 import FullArticle from '../FullArticle/FullArticle.js';
+import BrowseArticles from '../BrowseArticles/BrowseArticles.js';
 import { CircularProgress } from '@material-ui/core';
-
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import 'normalize.css';
 
 function App() {
   const [ articles, setArticles ] = useState([]);
+  const [ searchInput, setSearchInput ] = useState('')
   const [ error, setError ] = useState('');
 
   const getArticles = async () => {
@@ -22,6 +23,15 @@ function App() {
     }
   }
 
+  const getSearchResults = async (e) => {
+    try {
+      const stories = await fetchUSStories(e.target.value);
+      setSearchInput(stories);
+    } catch {
+      setError(`We were unable to find any results for "${e.target.value}". Please refine your search.`)
+    }
+  }
+
   useEffect(() => {
     getArticles();
   } , []);
@@ -29,11 +39,15 @@ function App() {
 
   return (
     <>
-      <Header />
+      <Header getSearchResults={getSearchResults} />
       {error && <h2 className="app_error">⚠️ {error}</h2>}
       {!error &&
       <main className="app_main">
         <Switch>
+          <Route exact path="/stories">
+            {!articles.length ? <CircularProgress /> :
+            <BrowseArticles articles={articles} />}
+          </Route>
           <Route exact path="/">
             {!articles.length ? <CircularProgress /> :
             <Dashboard articles={articles} />}
