9f565a786127e4b96394d42e15b3b08fdb6bca17
Reggie Thompson <reggie.thompson4@gmail.com>
2021-06-03 16:27:25 -0600

    Add submitSearch functionality to search button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,33 @@
+// import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 
-const Header = ({ getSearchResults }) => {
-  // const handleSearch = (e) => {
-  //   getSearchResults(e);
-  // }
+const Header = ({ getSearchResults, handleSearch, navToViewStories }) => {
+  const submitSearch = () => {
+    getSearchResults();
+  }
 
   return (
     <header className="header-app_header">
       <div className="header-search_container">
         <SearchIcon />
-        <input className="header_search-input" type="text" placeholder="Search by Article"></input>
-        <button className="header_search-button">Search</button>
+        <input 
+          className="header_search-input"
+          type="text" 
+          placeholder="Search by Article"
+          onChange={(event) => handleSearch(event)}
+          />
+        <Link to="stories">
+          <button 
+            className="header_search-button" 
+            onClick={() => submitSearch()}>
+            Search
+          </button>
+        </Link>
       </div>
-      <Link to="/stories" className="header_stories-link">
-        View Stories
+      <Link to="/stories" className="header_stories-link" onClick={() => navToViewStories()}>
+        View All Stories
       </Link>
     </header>
   )
